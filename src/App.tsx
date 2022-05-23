4029e581eede46ffa838a6256b9cfbba31c82265
ilya Slashchuk <ralfun11@me.com>
2022-05-23 02:09:13 +0200

    amendments

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,48 @@
-import React, { useState } from 'react';
-import "./App.css"
-import {Banner, Position} from "./components/banner/Banner";
-
-type MouseCoordinates = {
-  x: number,
-  y: number
-}
+import React, { useState } from "react";
+import "./App.css";
+import { Banner, Position } from "./components/banner/Banner";
 
 type BannerType = {
   position: Position,
-  id: number
-}
+  id: number,
+};
 
 function App() {
-  const [fileData, setFileData] = useState<string | ArrayBuffer | null>()
-  const [initialPosition, setInitialPosition] = useState<MouseCoordinates>()
-  const [banners, setBanners] = useState<Array<BannerType>>([])
+  const [fileData, setFileData] = useState<string | ArrayBuffer | null>();
+  const [banners, setBanners] = useState<Array<BannerType>>([]);
 
-    const backgroundImage = fileData ? `url("${fileData}"` : ""
+    const backgroundImage = fileData ? `url("${fileData}"` : "";
 
     const onImageAdded = (img: File | undefined) => {
       if(!img){
-          return
+          return;
       }
 
         const reader = new FileReader();
-        reader.readAsDataURL(img)
+        reader.readAsDataURL(img);
         reader.addEventListener("load", () => {
-            setFileData(reader.result)
+            setFileData(reader.result);
         });
-    }
+    };
 
-    const onMouseDownHandler = (e:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const onClickHandler = (e:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
 
       if(fileData && e.currentTarget === e.target){
-        setInitialPosition({
-        y: e.clientY,
-        x: e.clientX
-      })
-    }}
+        const top = (e.clientY/window.innerHeight)*100;
+        const left = (e.clientX/window.innerWidth)*100;
 
-    const onMouseUpHandler = (e:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if(e.currentTarget !== e.target){
-      return;
-    }
+        setBanners([...banners, {
+          position: {
+            top,
+            left,
+          },
+          id:left*top,
+        }])
+      }
 
-    if(Math.abs(initialPosition!.x - e.clientX) < 25 || Math.abs(initialPosition!.y - e.clientY) < 25){
-      setInitialPosition(undefined)
-      return
-    }
+    };
 
-    if(fileData){
-      const moveToTheRight = e.clientX > initialPosition!.x
-      const moveToTheBottom = e.clientY > initialPosition!.y
-
-      const top = ((moveToTheBottom ? initialPosition!.y : e.clientY)/window.innerHeight)*100;
-      const bottom = ((moveToTheBottom ? window.innerHeight - e.clientY : window.innerHeight - initialPosition!.y)/window.innerHeight)*100;
-      const right = ((moveToTheRight ? window.innerWidth - e.clientX : window.innerWidth - initialPosition!.x)/window.innerWidth)*100;
-      const left = ((moveToTheRight ? initialPosition!.x : e.clientX)/window.innerWidth)*100
-
-      banners.push({
-        position: {
-          top,
-          left,
-          right,
-          bottom,
-        },
-        id:left*top*bottom*right,
-      })
-      setInitialPosition(undefined)
-    }
-  }
-
-    const findAndDeleteBanner = (id:number) => setBanners(banners.filter((banner) => banner.id !== id))
+    const findAndDeleteBanner = (id:number) => setBanners(banners.filter((banner) => banner.id !== id));
 
   if(!fileData){
     return (
@@ -82,15 +52,24 @@ function App() {
               alt={"placeholder"}
               onInputCapture={(event) => onImageAdded(event.currentTarget.files?.[0])}
           />
+            <div>
+                <p>Double click on banner removes it</p>
+            </div>
         </div>
-          )
+    );
   }
 
   return (
-    <div onMouseDown={onMouseDownHandler} onMouseUp={onMouseUpHandler} className={"backgroundImage"} style={{
-     backgroundImage
-    }} >
-        {fileData && banners.map((banner) => <Banner position={banner.position} id={banner.id} remove={findAndDeleteBanner} key={banner.id} />)}
+    <div
+        onClick={onClickHandler}
+        className={"backgroundImage"}
+        style={{
+          backgroundImage
+        }}
+    >
+        {
+          fileData && banners.map((banner) => <Banner position={banner.position} id={banner.id} remove={findAndDeleteBanner} key={banner.id} />)
+        }
     </div>
   );
 }
--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,39 +1,51 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from "react"
 import "../../App.css"
 
 export type Position = {
     top: number,
     left: number,
-    right: number,
-    bottom: number
-}
+};
+
 export type BannerProps = {
     position: Position,
     id: number,
     remove: (id: number) => void
-}
+};
 
 export const Banner = ({position, id, remove}:BannerProps) => {
 
-    const [value, setValue] = useState<string>("")
+    const [value, setValue] = useState<string>("");
+
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            const elements = document.getElementsByTagName("textarea");
+            Array.from(elements).map((element) => {
+                element.style.height = "auto";
+                element.style.height = calculatePercentageHeight(element.scrollHeight);
+            })
+        });
+    });
 
     return (
-        <div
-            className="banner-container"
-            style={{
-                left: `${position.left}%`,
-                top: `${position.top}%`,
-                bottom: `${position.bottom}%`,
-                right: `${position.right}%`,
-            }}
-            onDoubleClick={() => remove(id)}
-        >
-        <input
-            type="text"
+        <textarea
             className="input-field"
             onChange={(e) => setValue(e.currentTarget.value)}
             value={value}
-            onClick={() => setValue("")} />
-        </div>
-    )
-}
+            onClick={() => setValue("")}
+            style={{
+                left: `${position.left}%`,
+                top: `${position.top}%`,
+                width: "7%",
+                height: "auto",
+            }}
+            onDoubleClick={() => remove(id)}
+            onInput={(e) => {
+                const element = e.currentTarget;
+                element.style.height = "auto";
+                element.style.height = calculatePercentageHeight(element.scrollHeight);
+            }}
+        />
+    );
+};
+
+const calculatePercentageHeight = (scrollHeight:number) => `${(( scrollHeight)/window.innerHeight)*100}%`
